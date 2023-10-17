4302ac1d6476f4566933617376ac022c5f41ab21
aamiraminali <aamir.amin@logicielservice.com>
2023-10-17 21:43:25 +0200

    Removed unused references from CRUD.js and DeleteEmployee.js

--- ems-frontend/src/Modules/Employee/DeleteEmployee.js
+++ ems-frontend/src/Modules/Employee/DeleteEmployee.js
@@ -2,9 +2,6 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 
 export default function DeleteEmployee(props){
 
